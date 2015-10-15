d8a14e6b178e50137df150513f77b28ad6516217
Chen Liu <chen.liu@cygni.se>
2015-10-15 21:06:37 +0200

    finished excise1-4

--- src/main/java/se/cygni/training/maven/App.java
+++ src/main/java/se/cygni/training/maven/App.java
@@ -1,12 +1,18 @@
 package se.cygni.training.maven;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Main class
  */
 public class App {
 
+    static Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
-        System.out.println("Starting up");
+        //System.out.println("Starting up");
+        logger.debug("Starting up,hello!!!");
         StringGetter getter = new StringGetter();
         System.out.println(getter.getStringFromResource());
     }
